4ee509b334be67d2b666a79c8c75020499bebf63
Dumitras <Caraseni.dumitras@gmai.com>
2023-06-12 17:07:11 +0300

    Add Faq model

---
+++ src/Presentation/Nop.Web/Areas/Admin/Models/FAQs/FaqModel.cs
@@ -0,0 +1,18 @@
+using Nop.Web.Framework.Models;
+using Nop.Web.Framework.Mvc.ModelBinding;
+
+namespace Nop.Web.Areas.Admin.Models.FAQs;
+
+public record FaqModel : BaseNopEntityModel
+{
+    [NopResourceDisplayName("Admin.FAQs.Faq.Fields.QuestionTitle")]
+    public string QuestionTitle       { get; set; }
+    [NopResourceDisplayName("Admin.FAQs.Faq.Fields.QuestionDescription")]
+    public string QuestionDescription { get; set; }
+    [NopResourceDisplayName("Admin.FAQs.Faq.Fields.AnswerTitle")]
+    public string AnswerTitle         { get; set; }
+    [NopResourceDisplayName("Admin.FAQs.Faq.Fields.AnswerDescription")]
+    public string AnswerDescription   { get; set; }
+    [NopResourceDisplayName("Admin.FAQs.Faq.Fields.CategoryId")]
+    public int    CategoryId          { get; set; }
+}
